26ee1377e435a68f547fc8920e2ce6eb4a1c2d28
Shetoraz <shetoraz@holyman.local>
2020-02-09 21:46:05 +0300

    Design reworked;

--- testWeather/Main Scene/ViewController.swift
+++ testWeather/Main Scene/ViewController.swift
@@ -9,19 +9,16 @@ class ViewController: UIViewController, LocationServiceDelegate {
     @IBOutlet weak var weekdayLabel: UILabel!
     @IBOutlet weak var maxTempLabel: UILabel!
     @IBOutlet weak var minTempLabel: UILabel!
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var slider: UILabel!
     
     private let locationManager = LocationService.shared
     private let networker = Networker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        LocationService.shared.delegate = self
+        locationManager.delegate = self
         networker.delegate = self 
-        tableView.separatorStyle = .none
-        tableView.backgroundColor = .none
-        collectionView.backgroundColor = .none
     }
     
     func onLocationUpdate(location: CLLocation) {
@@ -31,18 +28,28 @@ class ViewController: UIViewController, LocationServiceDelegate {
     func onLocationDidFailWithError(error: Error) {
         print("Error!")
     }
-    
-    //MARK: - Update UI
+
     func updateUI(city: String, status: String, currTemp: String, maxTemp: String, minTemp: String) {
         DispatchQueue.main.async {
             self.cityLabel.text = city
             self.statusLabel.text = status
             self.currentlyTemp.text = currTemp + "°"
-            self.maxTempLabel.text = maxTemp
-            self.minTempLabel.text = minTemp
+            self.maxTempLabel.text = maxTemp + "°"
+            self.minTempLabel.text = minTemp + "°"
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "EEEE"
             self.weekdayLabel.text = dateFormatter.string(from: Date())
+        }
+    }
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        switch sender.value {
+        case 1...12: 
+            timeLabel.text = String(Int(sender.value)) + " am"
+        case 12...24:
+            timeLabel.text = String(Int(sender.value)) + " pm"
+        default:
+            timeLabel.text = ""
         }
     }
 }
