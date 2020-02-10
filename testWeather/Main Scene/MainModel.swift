02eb7f9c17f40a86fcb40cbf2d829e94a9a8d8f4
Shetoraz <shetoraz@holyman.local>
2020-02-10 18:56:12 +0300

    Added slider functions; Beta model;  Should be fixed

---
+++ testWeather/Main Scene/MainModel.swift
@@ -0,0 +1,14 @@
+//
+//  MainModel.swift
+//  testWeather
+//
+//  Created by Anton Asetski on 2/10/20.
+//  Copyright © 2020 Anton Asetski. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    var hourWeather = [Int]()
+    
+}
--- testWeather/Main Scene/ViewController.swift
+++ testWeather/Main Scene/ViewController.swift
@@ -10,25 +10,26 @@ class ViewController: UIViewController, LocationServiceDelegate {
     @IBOutlet weak var maxTempLabel: UILabel!
     @IBOutlet weak var minTempLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var slider: UILabel!
+    @IBOutlet weak var slider: UISlider!
     
     private let locationManager = LocationService.shared
     private let networker = Networker()
+    private let model = Model()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationManager.delegate = self
-        networker.delegate = self 
+        self.locationManager.delegate = self
+        self.networker.delegate = self        
     }
     
     func onLocationUpdate(location: CLLocation) {
-        networker.sentRequest(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        self.networker.sentRequest(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
     }
     
     func onLocationDidFailWithError(error: Error) {
         print("Error!")
     }
-
+    
     func updateUI(city: String, status: String, currTemp: String, maxTemp: String, minTemp: String) {
         DispatchQueue.main.async {
             self.cityLabel.text = city
@@ -36,21 +37,37 @@ class ViewController: UIViewController, LocationServiceDelegate {
             self.currentlyTemp.text = currTemp + "°"
             self.maxTempLabel.text = maxTemp + "°"
             self.minTempLabel.text = minTemp + "°"
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "EEEE"
-            self.weekdayLabel.text = dateFormatter.string(from: Date())
+            self.updateDate()
         }
+    }
+    
+    func updateDate() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        self.weekdayLabel.text = dateFormatter.string(from: Date())
+        let hour = Calendar.current.component(.hour, from: Date())
+        self.slider.value = Float(hour)
+        switch hour {
+        case 1...12: 
+            self.timeLabel.text = String(Int(self.slider.value)) + " am"
+        case 12...24:
+            self.timeLabel.text = String(Int(self.slider.value)) + " pm"
+        default:
+            self.timeLabel.text = ""
+        }
+        self.slider.minimumValue = Float(hour)
     }
     
     @IBAction func sliderMoved(_ sender: UISlider) {
         switch sender.value {
         case 1...12: 
-            timeLabel.text = String(Int(sender.value)) + " am"
+            self.timeLabel.text = String(Int(sender.value)) + " am"
         case 12...24:
-            timeLabel.text = String(Int(sender.value)) + " pm"
+            self.timeLabel.text = String(Int(sender.value)) + " pm"
         default:
-            timeLabel.text = ""
+            self.timeLabel.text = ""
         }
+        self.currentlyTemp.text = String(self.model.hourWeather[Int(sender.value-1)])
     }
 }
 
@@ -58,5 +75,9 @@ extension ViewController: NetworkDelegate {
     
     func didReceiveData(_ data: Welcome) {
         updateUI(city: data.timezone, status: data.currently.summary, currTemp: String(Int(data.currently.temperature)), maxTemp: String(Int(data.daily.data[0].temperatureMax)), minTemp: String(Int(data.daily.data[0].temperatureLow)))
+        
+        for hour in data.daily.data {
+            self.model.hourWeather.append(Int(hour.temperatureHigh))
+        }
     }
 }
